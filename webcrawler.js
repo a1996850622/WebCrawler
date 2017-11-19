57a9e11055b62fdb6bacf775d6a9f38d891be5af
a1996850622 <a1996850622@gmail.com>
2017-11-19 23:48:40 +0800

    Update

--- webcrawler.js
+++ webcrawler.js
@@ -1,25 +1,56 @@
-var request = require("request");
-var cheerio = require("cheerio");
+var request = require('request');
+var cheerio = require('cheerio');
 var fs = require('fs');
 
 var url = "https://www.esunbank.com.tw/bank/personal/deposit/rate/forex/foreign-exchange-rates";
 
-/* Get the bank webpage */
-request(url, function(error1, response, body){
-	if(!error1){
-		/* Write the webpage as a file */
-		fs.writeFile("WebData.txt", body, function(error2){
-			if(error2){
-				console.log(error2);
-			} else{
-				console.log("The webpage was saved!");
-			}
-		});
+/* Get the data the webpage */
+request(url, function (error, response, body) {
+    if (!error) {
 
-		var parse = cheerio.load(body);
-		console.log(body);
+    	// console.log(body);
 
-	} else{
-		console.log(error1);
-	}
+    	/* Save the source code of the webpage */
+    	fs.writeFile('WebData.txt', body, function(err){
+    		if(err){
+    			console.log(err);
+    		} else{
+    			console.log('The webpage was saved!');
+    		}
+    	});
+
+        /* Parse the html data */
+        var $ = cheerio.load(body);
+
+        /* Search for exchange rate in various currencies */
+        var queryResult = [];
+
+        /* Catch the data of exchange rate */
+        $('.inteTable tr.tableContent-light').each(function(i, elem){
+        	/* Save the result to queryResult */
+            queryResult.push($(this).text().split('\n'));
+        });
+
+        // console.log(queryResult);
+
+        /* Sort the result of search */
+        var output = [];
+        for(var i=0; i<queryResult.length; i++) {
+            output.push({
+            	/* currency */
+                coin: queryResult[i][2].replace(/\s/g, ''),
+
+                /* Buy in */
+                in: queryResult[i][6].replace(/\s/g, ''),
+
+                /* Sell out */
+                out: queryResult[i][7].replace(/\s/g, '')
+            });
+        }
+        console.log(output);
+    } 
+
+    else {
+        console.log(error);
+    }
 });
